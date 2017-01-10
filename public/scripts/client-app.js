c899aa78e0e1d7698a9cbc976a689e8b57ac660c
Kati Lue <katillueth@gmail.com>
2017-01-10 14:36:27 -0600

    controllers added

--- public/scripts/client-app.js
+++ public/scripts/client-app.js
@@ -2,31 +2,26 @@ var myApp = angular.module('myApp', ['ngRoute']);
 
 myApp.config(['$routeProvider', function($routeProvider){
   $routeProvider
-    //.when('/nav', {
-    //  templateUrl: '/views/templates/index.html',
-    //  controller: 'NavController'
-    //})
-    //.when('/footer', {
-    //  templateUrl: '/views/templates/footer.html',
-    //})
-    //.when('/about', {
-    //  templateUrl: '/views/templates/about.html',
-    //  controller: 'AboutController'
-    //})
+    .when('/nav', {
+      templateUrl: '/views/templates/index.html',
+      controller: 'NavController'
+    })
+    .when('/footer', {
+      templateUrl: '/views/templates/footer.html',
+    })
+    .when('/about', {
+      templateUrl: '/views/templates/about.html',
+      controller: 'AboutController'
+    })
     .when('/home', {
       templateUrl: '/views/templates/home.html',
       controller: 'HomeController'
     })
-    //.when('/services', {
-    //  templateUrl: '/views/templates/services.html',
-    //  controller: 'ServicesController'
-    //})
+    .when('/services', {
+      templateUrl: '/views/templates/services.html',
+      controller: 'ServicesController'
+    })
     .otherwise({
       redirectTo: 'home',
     })
-
-
-
-
-
 }]);
